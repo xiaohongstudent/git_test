20a15fa3215102afc1f11cdcc6d13418f59a0e20
xiaohongstudent <48013302+xiaohongstudent@users.noreply.github.com>
2019-04-12 10:19:47 +0800

    Update GitTest.java

--- src/main/java/git_test/GitTest.java
+++ src/main/java/git_test/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
 
 	public static void main(String[] args) {
 		System.out.println("hello world git master branch merge taolong branch");
+		System.out.println("modify by github");
 	}
 }
