9b2d01f339fc5e287cefdf997bfb736675ad4dd3
hongtaolong <hongtaolong1030@163.com>
2019-04-12 10:09:26 +0800

    Revert "v4.0"
    This reverts commit 0d00b1a236c94b5e631b9dc5d7b2fede494b6bae.

--- src/main/java/git_test/GitTest.java
+++ src/main/java/git_test/GitTest.java
@@ -3,6 +3,6 @@ package git_test;
 public class GitTest {
 
 	public static void main(String[] args) {
-		System.out.println("hello world git master branch merge taolong branch revert test");
+		System.out.println("hello world git master branch merge taolong branch");
 	}
 }
